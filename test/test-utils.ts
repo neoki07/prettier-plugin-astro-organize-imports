1dfb80fa0d5a0a9bf81eb7236e45a213c99ee405
ot07 <ot07neoki@gmail.com>
2023-06-25 14:15:36 +0900

    test: 💍 update test-utils.ts

--- test/test-utils.ts
+++ test/test-utils.ts
@@ -5,12 +5,11 @@ import { expect, it } from 'vitest';
 /**
  * format the contents of an astro file
  */
-export function format(contents: string, options: prettier.Options = {}): string {
+export function format(contents: string): string {
 	try {
 		return prettier.format(contents, {
 			parser: 'astro',
 			plugins: [fileURLToPath(new URL('../', import.meta.url).toString())],
-			...options,
 		});
 	} catch (e) {
 		if (e instanceof Error) {
@@ -36,20 +35,6 @@ function getFiles(file: any, path: string) {
 	return { input, output };
 }
 
-function getOptions(files: any, path: string) {
-	if (files[`/test/fixtures/${path}/options.js`] !== undefined) {
-		return files[`/test/fixtures/${path}/options.js`].default;
-	}
-
-	let opts: object;
-	try {
-		opts = JSON.parse(files[`/test/fixtures/${path}/options.json`]);
-	} catch (e) {
-		opts = {};
-	}
-	return opts;
-}
-
 /**
  * @param {string} name Test name.
  * @param {any} files Files from import.meta.glob.
@@ -62,29 +47,11 @@ export function test(name: string, files: any, path: string) {
 		expect(input, 'Missing input file').to.not.be.undefined;
 		expect(output, 'Missing output file').to.not.be.undefined;
 
-		const opts = getOptions(files, path);
-
-		const formatted = format(input, opts);
-		const formattedBuffer = Buffer.from(formatted);
-		const outputBuffer = Buffer.from(output);
-
-		if (Buffer.compare(formattedBuffer, outputBuffer) !== 0) {
-			console.log('formatted:', formattedBuffer);
-			console.log('output:', outputBuffer);
-			throw new Error('Buffers are not equal');
-		}
-
+		const formatted = format(input);
 		expect(formatted, 'Incorrect formatting').toBe(output);
 
 		// test that our formatting is idempotent
-		const formattedTwice = format(formatted, opts);
-		const formattedTwiceBuffer = Buffer.from(formattedTwice);
-
-		if (Buffer.compare(formattedTwiceBuffer, outputBuffer) !== 0) {
-			console.log('formatted(twice):', formattedTwiceBuffer);
-			console.log('output:', outputBuffer);
-			throw new Error('Buffers are not equal');
-		}
+		const formattedTwice = format(formatted);
 		expect(formatted === formattedTwice, 'Formatting is not idempotent').toBe(true);
 	});
 }
