856a195f2c270a0b730bcd4cad7c69f45cd0bae8
ot07 <ot07neoki@gmail.com>
2023-09-16 00:52:56 +0900

    remove unused esbuild plugin

--- build.mjs
+++ build.mjs
@@ -6,33 +6,6 @@ import esbuild from 'esbuild'
 /**
  * @returns {import('esbuild').Plugin}
  */
-function patchRecast() {
-  return {
-    // https://github.com/benjamn/recast/issues/611
-    name: 'patch-recast',
-    setup(build) {
-      build.onLoad({ filter: /recast\/lib\/patcher\.js$/ }, async (args) => {
-        let original = await fs.promises.readFile(args.path, 'utf8')
-
-        return {
-          contents: original
-            .replace(
-              'var nls = needsLeadingSpace(lines, oldNode.loc, newLines);',
-              'var nls = oldNode.type !== "TemplateElement" && needsLeadingSpace(lines, oldNode.loc, newLines);',
-            )
-            .replace(
-              'var nts = needsTrailingSpace(lines, oldNode.loc, newLines)',
-              'var nts = oldNode.type !== "TemplateElement" && needsTrailingSpace(lines, oldNode.loc, newLines)',
-            ),
-        }
-      })
-    },
-  }
-}
-
-/**
- * @returns {import('esbuild').Plugin}
- */
 function patchCjsInterop() {
   return {
     name: 'patch-cjs-interop',
@@ -90,7 +63,7 @@ let context = await esbuild.context({
   entryPoints: [path.resolve(__dirname, './src/index.js')],
   outfile: path.resolve(__dirname, './dist/index.mjs'),
   format: 'esm',
-  plugins: [patchRecast(), patchCjsInterop(), copyTypes()],
+  plugins: [patchCjsInterop(), copyTypes()],
 })
 
 await context.rebuild()
